8100a9b4f1a33342dd2ab29f74487166d8a1ddea
user <user@DESKTOP-3NLRG24>
2022-10-31 09:18:51 +0100

    Add project files.

---
+++ WebApplication4/Controllers/loginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class loginController : Controller
+    {
+        public IActionResult Index(string uname, string psw)
+        {
+
+            person p1 = new person(uname, psw);
+
+            return View(p1);
+        }
+        public IActionResult page()
+        {
+            return View();
+        }
+        public IActionResult loginpage()
+        {
+            
+
+
+            return View();
+        }
+        public IActionResult register()
+        {
+            return View();
+        }
+        public IActionResult dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/Models/person.cs
@@ -0,0 +1,60 @@
+﻿namespace WebApplication4.Models
+{
+    public class person
+    {
+
+
+        public string userName;
+        public string password;
+
+        public List<string> usernames = new List<string> { "john", "pascal", "daniel" };
+        public List<string> password_list = new List<string> { "123", "123", "123" };
+        public List<decimal> acc_bal = new List<decimal> { 20000, 40000, 30000 };
+
+        public person(string username, string pword)
+        {
+            this.userName = username;
+            this.password = pword;
+
+        }
+
+      
+        public string login2()
+        {
+            int Index = usernames.IndexOf(userName);
+            Console.WriteLine(Index);
+            string message;
+
+            if (Index == -1)
+            {
+                message = "does not exist";
+                return message;
+
+            }
+            else
+            {
+                string pwd = password_list[Index];
+
+               
+
+                if (pwd == password)
+                {
+                    message = $"hello welcome   {userName}";
+                    return message;
+
+
+
+
+
+                }
+                else
+                {
+                    
+                    message = "password does not exist";
+                    return message;
+                }
+            }
+        }
+
+    }
+}
